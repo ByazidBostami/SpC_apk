229b3d476839a5ce91ac6a89c6d5668a2cde3f03
Byazid <byazid@Byazids-MacBook-Air.local>
2025-07-10 00:52:10 +0600

    apk done

---
+++ app/src/main/java/com/tsi/sports/MainActivity.java
@@ -0,0 +1,19 @@
+
+package com.tsi.sports;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://sportsportal.tsi-bd.com/");
+        setContentView(webView);
+    }
+}
